7b1df2a4015035e080398564cc26aad95db65554
Quan Nguyen <quanwnn@gmail.com>
2022-06-01 19:01:56 -0400

    create current_cart concern to persist cart in a session

---
+++ app/controllers/concerns/current_cart.rb
@@ -0,0 +1,17 @@
+# Rails concern allows us to share codes in this module among controllers
+module CurrentCart
+
+    private
+
+    # this method helps us persist the shopping cart in a session
+    def set_cart
+        # get the cart_id from the session object and then find a cart corresponding to this ID
+        @cart = Cart.find(session[:cart_id])
+    
+    # if ID not found, create a cart object and then store that cart's id into the session object
+    rescue ActiveRecord::RecordNotFound
+        @cart = Cart.create
+        session[:cart_id] = @cart.id
+    end
+
+end
