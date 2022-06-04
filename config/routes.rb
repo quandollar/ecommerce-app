aca40d6e8e111e9f5d7ada6779eb88671a9d7b06
Quan Nguyen <quanwnn@gmail.com>
2022-06-04 16:12:57 -0400

    add about page and favicon

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
+  root 'store#index', as: 'store_index'
   resources :orders
   resources :line_items
   resources :carts
-  root 'store#index', as: 'store_index'
   resources :products
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "about", to: "pages#about"
 end
